54a245871bec33469efab28571b795c10ee8e346
NareshK <nareshyadav.k1996@gmail.com>
2023-05-26 16:17:00 -0500

    first Commit

---
+++ src/main/java/com/main/SpringBootEmployeeAssociationMapping01Application.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEmployeeAssociationMapping01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEmployeeAssociationMapping01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/SpringBootEmployeeAssociationMapping01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEmployeeAssociationMapping01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
